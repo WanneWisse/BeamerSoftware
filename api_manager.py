723ac752830f0925f581cf7b15e44c2765be926e
Niels <nielsgroeneveld8@gmail.com>
2023-01-15 15:49:55 +0100

    applied linter

--- api_manager.py
+++ api_manager.py
@@ -3,24 +3,30 @@ from lyricsgenius import Genius
 from bs4 import BeautifulSoup
 import bs4
 
-class ApiManager():
+
+class ApiManager:
     def __init__(self, api_key):
         self.api_key = api_key
         self.genius = self.init_api()
+
     def init_api(self):
         genius = Genius(self.api_key)
         genius.verbose = False
         return genius
+
     def get_pages_with_lyrics(self, title, artist):
-        song_lyrics_url = self.get_song_lyrics_url(title,artist)
+        song_lyrics_url = self.get_song_lyrics_url(title, artist)
         lyrics_from_webpage = self.webscrape_song_lyrics_url(song_lyrics_url)
         lyrics_without_headers = self.delete_lyrics_headers(lyrics_from_webpage)
         pages_of_lyrics = self.create_pages(lyrics_without_headers)
         return pages_of_lyrics
-    def get_song_lyrics_url(self,title,artist):
+
+    def get_song_lyrics_url(self, title, artist):
         song_info = self.genius.search_song(title=title, artist=artist, song_id=None, get_full_info=True)
         return song_info.to_dict()["url"]
-    def webscrape_song_lyrics_url(self, url):
+
+    @staticmethod
+    def webscrape_song_lyrics_url(url):
         r = requests.get(url, allow_redirects=True)
         # open("page.html", "wb").write(r.content)
         # text = None
@@ -30,7 +36,7 @@ class ApiManager():
 
         lyrics = []
         for i in soup.find_all("div", class_="Lyrics__Container-sc-1ynbvzw-6 YYrds"):
-            
+
             for tag in i.contents:
                 if type(tag) == bs4.element.Tag:
                     if tag.name == "br":
@@ -48,14 +54,16 @@ class ApiManager():
                     lyrics.append(tag.string)
         return lyrics
 
-    def delete_lyrics_headers(self,lyrics):
+    @staticmethod
+    def delete_lyrics_headers(lyrics):
         new_lyrics = []
         for index in range(len(lyrics)):
             if lyrics[index][0] != "[":
                 new_lyrics.append(lyrics[index])
         return new_lyrics
 
-    def create_pages(self,lyrics):
+    @staticmethod
+    def create_pages(lyrics):
         pages_with_text = []
 
         amount_n = 0
@@ -63,7 +71,7 @@ class ApiManager():
         for characters in lyrics:
             if characters == "\n":
                 if current_text_page != "":
-                    amount_n +=1
+                    amount_n += 1
                     current_text_page += characters
             else:
                 amount_n = 0
@@ -74,11 +82,3 @@ class ApiManager():
                 amount_n = 0
         pages_with_text.append(current_text_page)
         return pages_with_text
-
-
-
-
-
-
-
-
--- gui.py
+++ gui.py
@@ -4,60 +4,63 @@ from tkinter import *
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 api_manager = ApiManager(os.getenv("GENIUS_API_KEY"))
+
 
 def search():
     global index_text
     global pages_with_text
     artist_to_search = artist.get()
     song_to_search = song_name.get()
-    pages_with_text = api_manager.get_pages_with_lyrics(song_to_search,artist_to_search)
-    index_text = 0 
+    pages_with_text = api_manager.get_pages_with_lyrics(song_to_search, artist_to_search)
+    index_text = 0
     current_text = pages_with_text[0]
     music_show_text.config(text=current_text)
     music_show_frame.pack()
     music_show_frame.focus_set()
     search_music_frame.forget()
 
+
 def stop(event):
     music_show_frame.forget()
     search_music_frame.pack()
     search_music_frame.focus_set()
 
+
 def leftKey(event):
     global index_text
     if index_text > 0:
-        index_text -=1
+        index_text -= 1
         current_text = pages_with_text[index_text]
-        music_show_text.config(text = current_text)
-    
+        music_show_text.config(text=current_text)
+
 
 def rightKey(event):
     global index_text
-    if index_text < len(pages_with_text)-1:
-        index_text +=1
+    if index_text < len(pages_with_text) - 1:
+        index_text += 1
         current_text = pages_with_text[index_text]
-        music_show_text.config(text = current_text)
+        music_show_text.config(text=current_text)
+
 
 pages_with_text = []
-index_text = 0 
+index_text = 0
 current_text = ""
 
 window = Tk()
 window.attributes("-fullscreen", True)
-font = tkFont.Font(size = 50)
+font = tkFont.Font(size=50)
 
-search_music_frame=Frame(window)
+search_music_frame = Frame(window)
 
-song_name_label = Label(search_music_frame, text="Song Name", font = font)
+song_name_label = Label(search_music_frame, text="Song Name", font=font)
 song_name = Entry(search_music_frame, font=font)
 
-artist_label = Label(search_music_frame, text="Artist", font = font)
-artist = Entry(search_music_frame, font = font)
+artist_label = Label(search_music_frame, text="Artist", font=font)
+artist = Entry(search_music_frame, font=font)
 
-search = Button(search_music_frame, text ="search", command = search, font=font)
+search = Button(search_music_frame, text="search", command=search, font=font)
 
 song_name_label.pack()
 song_name.pack()
@@ -69,7 +72,7 @@ search_music_frame.pack()
 
 music_show_frame = Frame(window)
 
-music_show_text = Label(music_show_frame, text=current_text, font = font)
+music_show_text = Label(music_show_frame, text=current_text, font=font)
 music_show_text.pack()
 music_show_frame.bind('<Left>', leftKey)
 music_show_frame.bind('<Right>', rightKey)
